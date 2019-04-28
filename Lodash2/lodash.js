7cba7d5abfbeae4f49ec22f91f415e8c2ebdca72
L1237 <49970990+L1237@users.noreply.github.com>
2019-04-28 15:18:34 +0200

    Codecademy generated from the project submission page.

--- Lodash2/lodash.js
+++ Lodash2/lodash.js
@@ -1,16 +1,17 @@
 const _ = {
-  clamp(number, lower, upper){
+  
+clamp(number, lower, upper){
   let lowerClampedValue = Math.max(number,lower);
   let clampedValue = Math.min(lowerClampedValue,upper);
-    return clampedValue;
+  return clampedValue;
   },
   
   inRange(number ,start, end){ 
     if (end == undefined){
-     end = start
-     start = 0  
+    end = start
+    start = 0  
     }
-    if(start > end){
+ if(start > end){
     var temp = end 
     end = start
     start = temp
@@ -81,10 +82,3 @@ const _ = {
     }
   
     
-    
-    
- 
-
-
-// Do not write or modify code below this line.
-module.exports = _;
